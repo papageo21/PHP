2234db57d69175f93be94da0d95df18779f1e05f
George <77338393+papageo21@users.noreply.github.com>
2021-12-27 19:26:34 +0200

    Create array.php

---
+++ array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>test</title>
+</head>
+
+<body>
+  <h1>PHP</h1>
+  <?php 
+    $student = [[
+      'name' => 'Maria','age' => 20,'grade' => 8.5,
+      'name' => 'John','age' => 20,'grade' => 8.5,
+      'name' => 'George','age' => 20,'grade' => 8.5,
+      'name' => 'Anna','age' => 20,'grade' => 8.5,
+      ]];
+
+      print_r($student);
+
+   ?>
+</body>
+
+</html>
