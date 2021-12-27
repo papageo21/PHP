7155a454c095a82e9f87018e978bde5049ac98f5
George <77338393+papageo21@users.noreply.github.com>
2021-12-27 19:24:53 +0200

    Create check.php

---
+++ check.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="el">
+<head>
+    <meta charset="UTF-8">
+    <title>thema2</title>
+</head>
+<body>
+<h1>Proodos thema 2 PHP</h1>
+<?php
+
+$grade = 60;
+
+if($grade < 50){
+    echo"<p style='color:red'>FAIL</p>";
+}
+else
+    echo"PASS";
+
+?>
+
+</body>
+</html>
